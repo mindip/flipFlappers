04dd88b523ca8eb24aabc1f15b1680ac08eaf43e
mindip <oacidnim@g.ucla.edu>
2018-01-21 02:45:47 -0800

    initial commit

---
+++ Assets/ReceivePosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceivePosition : MonoBehaviour {
+    
+   	public OSC osc;
+
+
+	// Use this for initialization
+	void Start () {
+		osc.SetAddressHandler( "/muse/elements/gamma_session_score" , OnReceiveGamma);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnReceiveGamma(OscMessage message) {
+		print (message);
+    }
+}
